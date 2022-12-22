90167881921ca3e8e68d65ace2fcca2352c28d9c
modzhin <modzhds@gmail.com>
2022-12-22 20:04:30 +0300

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if ((num > -1000 && num < -99) || (num > 99 && num < 1000))
+{
+    Console.Write($"Третье значение вашего числа = {num % 10}");
+}
+else
+{
+    Console.Write("Ошибка. Введенное число должно быть трехзначным.");
+}
