4d30d42b29f0b0b21b4ca891ad641932c59b7846
modzhin <modzhds@gmail.com>
2022-12-22 21:12:18 +0300

    task13

---
+++ task13/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int ind = 0;
+while (num <= -1000 || num >= 1000)
+{
+    num = num / 10;
+    ind++;
+}
+if (num <= -100 || num >= 100)
+{
+    Console.Write($"Третье значение вашего числа = {num % 10}.");
+}
+else
+{
+    Console.Write($"Ошибка, в числе должно быть не менее 3-х знаков.");
+}
+
