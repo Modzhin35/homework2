0157cb4e859511ca42a003def7b7bc8abcb63a92
modzhin <modzhds@gmail.com>
2022-12-22 22:52:44 +0300

    task15

---
+++ task15/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите любое число и узнайте, выходной ли это день: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 0) num = num * -1; //Если введено отрицательное число, меняем знак
+int resNum = num % 7;
+if (resNum == 0)
+{ //для того, чтобы значение resNum в выводе не выдавало "0", когда выпаадет воскресенье;
+    resNum = 7;
+}
+switch (resNum)
+{
+    case 6:
+    case 7:
+        Console.Write($"{num} день это {resNum} день недели - выходной");
+        break;
+    default:
+        Console.Write($"{num} день это {resNum} день недели - рабочий");
+        break;
+}
